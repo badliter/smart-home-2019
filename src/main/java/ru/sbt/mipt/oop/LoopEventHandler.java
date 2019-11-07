4d42ac035665a158949d36af6c08b4140045e229
Badanin Maxim <Badanin.M.A@sberbank.ru>
2019-11-07 19:09:43 +0300

    was return to hometask-3 realization with LoopEventHandler

--- src/main/java/ru/sbt/mipt/oop/LoopEventHandler.java
+++ src/main/java/ru/sbt/mipt/oop/LoopEventHandler.java
@@ -3,7 +3,17 @@ package ru.sbt.mipt.oop;
 import ru.sbt.mipt.oop.home.SmartHome;
 
 public class LoopEventHandler {
-    public void performLoopEventHandle(SmartHome smartHome, SensorEventReader sensorEventReader, EventProcess eventProcess) {
+    SmartHome smartHome;
+    SensorEventReader sensorEventReader;
+    EventProcess eventProcess;
+
+    public LoopEventHandler(SmartHome smartHome, SensorEventReader sensorEventReader, EventProcess eventProcess){
+        this.smartHome = smartHome;
+        this.sensorEventReader = sensorEventReader;
+        this.eventProcess = eventProcess;
+    }
+
+    public void performLoopEventHandle() {
         SensorEvent event = sensorEventReader.getNextSensorEvent();
         while (event != null) {
             eventProcess.processEvent(smartHome, event);
